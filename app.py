8b96832670a1fb1eb6e915f6b5de1be84436d03e
vdgarg529 <83596912+vdgarg529@users.noreply.github.com>
2021-06-09 13:21:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import pandas_datareader as web
 import numpy as np
 from requests.sessions import session
 from pickle import load
-#from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 import datetime
 today = datetime.date.today()
 span30 = datetime.timedelta(days=30)
