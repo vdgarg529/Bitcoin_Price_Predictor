9648fa6edf71fb8ccfdd03a97bf8b0408c16930d
vdgarg529 <83596912+vdgarg529@users.noreply.github.com>
2021-06-10 18:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,31 +20,6 @@ Prev_1_date=today-span1
 next_date=today+span1
 
 app=Flask(__name__)
-# NIFTY50_model=load_model("NIFTY50_model.h5")
-# NIFTY50_scaler=load(open("NIFTY50_scaler.pkl","rb"))
-# NIFTYBANK_model=load_model("NIFTYBANK_model.h5")
-# NIFTYBANK_scaler=load(open("NIFTYBANK_scaler.pkl","rb"))
-# NIFTYIT_model=load_model("NIFTYIT_model.h5")
-# NIFTYIT_scaler=load(open("NIFTYIT_scaler.pkl","rb"))
-# def predict(name): 
-#   quote=web.DataReader(name,data_source='yahoo',start='2012-01-01', end=str(Prev_1_date))
-#   new_df=quote.filter(['Close'])
-#   last_60_days=new_df.iloc[-60:].values
-#   last_60_days_scaled=scaler.transform(last_60_days)
-#   X_test=[]
-#   X_test.append(last_60_days_scaled)
-#   X_test=np.array(X_test)
-#   X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
-#   pred_price=model.predict(X_test)
-#   pred_price=scaler.inverse_transform(pred_price)
-#   return (pred_price)
-# def last15dates():
-#   l=[]
-#   for i in range(15):
-#     span= datetime.timedelta(days=i)
-#     Prevdate=today-span
-#     l.append(Prevdate)
-#   return l
 
 @app.route('/', methods=["GET"])
 def main():
@@ -86,45 +61,6 @@ def bitcoin():
         str(Next_30_date):int(loaded_model.predict(price_array))
     }
     return jsonify(result)
-# @app.route('/STOCKS/NIFTY50')
-# def NIFTY50():
-#     model=NIFTY50_model
-    
-#     test=test_data('^NSEI')
-#     next=predict('^NSEI')
-#     dte=last15dates()
-    
-#     result={
-#         "Type":"Stock-NIFTY-50",
-#         str(next_date):int(next)
-#     }
-#     return jsonify(result)
-# @app.route('/STOCKS/NIFTYBANK')
-# def NIFTYBANK():
-#     model=NIFTYBANK_model
-    
-#     test=test_data('^NSEBANK')
-#     next=predict('^NSEBANK')
-#     dte=last15dates()
-    
-#     result={
-#         "Type":"Stock-NIFTY-BANK",
-#         str(next_date):int(next)
-#     }
-#     return jsonify(result)
-# @app.route('/STOCKS/NIFTYIT')
-# def NIFTYIT():
-#     model=NIFTYIT_model
-    
-#     test=test_data('^CNXIT')
-#     next=predict('^CNXIT')
-#     dte=last15dates()
-    
-#     result={
-#         "Type":"Stock-NIFTY-IT",
-#         str(next_date):int(next)
-#     }
-#     return jsonify(result)
      
 if __name__=="__main__":
     app.run()
